3aea22f4bcc474105b313eda59ae858b76da83e5
Sanjay-RD <rishidevsanjay9@gmail.com>
2020-12-10 09:00:09 +0545

    Contact Update and Delete Routes

--- routes/contact.js
+++ routes/contact.js
@@ -73,21 +73,58 @@ router.post(
 // @desc      update contact
 // @route     PUT api/v1/contacts/:id
 // @access    Private
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: `update contact of id ${req.params.id}`,
-  });
+router.put("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    console.log(contact);
+    if (!contact) {
+      return res.status(400).json({
+        success: false,
+        msg: `There is no user with this id of ${req.params.id}`,
+      });
+    }
+
+    if (contact.user.toString() !== req.id) {
+      return res.status(400).json({
+        success: false,
+        msg: `Not Authorized to access this route`,
+      });
+    }
+
+    contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      success: true,
+      contact,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
 // @desc      Delete contact
 // @route     DELETE api/v1/contacts/:id
 // @access    Private
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: `Delete contact of id ${req.params.id}`,
-  });
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
 module.exports = router;
