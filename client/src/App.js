ddf35ef32223e0a84eb588e44185f5ccb6d1261d
Sanjay-RD <rishidevsanjay9@gmail.com>
2020-12-10 10:55:21 +0545

    Cleanup,Navbar & Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./component/layout/Header";
+import Home from "./component/pages/Home";
+import About from "./component/pages/About";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Header title="Contact Keeper" icon="fa fa-id-card-o" />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/component/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is contact keeper app to keep contact</p>
+      <small className="bg-dark p">Version:1.1.3</small>
+    </React.Fragment>
+  );
+}
