1a8908732c25f6963ba0de9cb089d93cf0146a26
Sanjay-RD <rishidevsanjay9@gmail.com>
2020-12-14 16:44:13 +0545

    Auth Context & Initial State

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Header from "./component/layout/Header";
 import Home from "./component/pages/Home";
 import About from "./component/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 import "./App.css";
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Header title="Contact Keeper" icon="fa fa-id-card-o" />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Header title="Contact Keeper" icon="fa fa-id-card-o" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
